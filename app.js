da9754263f632c5295bacf20fdb71b12f32969fa
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-03-18 09:43:12 +0100

    ✨ setings

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const nunjucks = require('nunjucks');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(
+    sassMiddleware({
+        src: path.join(__dirname, 'public'),
+        dest: path.join(__dirname, 'public'),
+        indentedSyntax: true, // true = .sass and false = .scss
+        sourceMap: true
+    })
+);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { sameSite: true }
+    })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
